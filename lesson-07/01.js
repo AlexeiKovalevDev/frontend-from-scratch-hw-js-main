f65baa12db9b10a3b6e59ca4a5a9d80a8942c038
AlexeiKovalevDev <leprkovalev@gmail.com>
2024-09-03 23:46:14 +0300

    lesson7.1

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -8,7 +8,7 @@
 
 function capitalizeWords(str) {
   const arr = str.split(' ')
-  for(let i = 0; i < str.length; i++){
+  for(let i = 0; i < arr.length; i++){
     arr= arr[i].charAt(0).toUpperCase() + arr[i].slice(1).toLowerCase()
   }
   return arr.join(' ')
